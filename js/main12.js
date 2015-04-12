f5237f0bf5717541c5eb21a00ecb17bf566bd408
Ido Green <greenido@gmail.com>
2015-04-12 12:31:26 +0300

    A better indication for offline/online mode

--- js/main12.js
+++ js/main12.js
@@ -8,3 +8,19 @@
 //
 //
 console.log("** We started the party in main12.js **");
+
+// Detect when we are online / offline
+//
+window.addEventListener('load', function() {
+  var status = document.getElementById("status");
+
+  function updateOnlineStatus(event) {
+    var condition = navigator.onLine ? "online" : "offline";
+
+    status.className = condition;
+    status.innerHTML = condition.toUpperCase();
+  }
+
+  window.addEventListener('online',  updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+});
